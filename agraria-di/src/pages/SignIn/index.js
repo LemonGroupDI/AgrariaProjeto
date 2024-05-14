016cb854eea5c6575b9e4360bf24c6546e61cc98
MilenaRickliS <141282223+MilenaRickliS@users.noreply.github.com>
2024-05-14 20:42:59 -0300

    c

--- agraria-di/src/pages/SignIn/index.js
+++ agraria-di/src/pages/SignIn/index.js
@@ -115,8 +115,15 @@
 //         </div>
 //     );
 // }
+import { db, auth } from '../../services/firebaseConnetion';
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+  } from 'firebase/auth';
+import { useState, useContext, useEffect } from 'react'
 
-import { useState, useContext } from 'react'
 import './style.css'
 
 import logo from '../../assets/logoAgrariaN.png'
@@ -126,19 +133,90 @@ import foto1 from '../../assets/gadodecorte.jpg'
 
 
 export default function SignIn(){
+    //Estado para armazenar o email e a senha do usuário.
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [senha, setSenha] = useState('');
+  // Estado para verificar se o usuário está logado.
+  const [user, setUser] = useState(false);
+  // Estado para armazenar os detalhes do usuário logado.
+  const [userDetail, setUserDetail] = useState({});
 
-  const { user, signIn, loadingAuth } = useContext(AuthContext)
-
-  async function handleSignIn(e){
-    e.preventDefault();
-
-    if(email !== '' && password !== ''){
-      await signIn(email, password);
+   // Efeito que verifica se o usuário está logado quando o componente é montado.
+   useEffect(() => {
+    async function checkLogin(){
+    onAuthStateChanged(auth, (user) => {
+    if(user){
+      console.log(user);
+      setUser(true);
+      setUserDetail({
+      uid: user.uid,
+      email: user.email
+    })
+    }else{
+      setUser(false);
+      setUserDetail({})
     }
+    })
+    }
+    checkLogin();
+  }, [])
 
+  // Função para criar um novo usuário no Firebase Auth.
+  async function novoUsuario(){
+    await createUserWithEmailAndPassword(auth, email, senha)
+    .then(() => {
+      console.log("CADASTRADO COM SUCESSO!");
+      setEmail('');
+      setSenha('');
+    })
+    .catch((error) => {
+      if(error.code === 'auth/weak-password'){
+      alert("Senha muito fraca.");
+    }else if(error.code === 'auth/email-already-in-use'){
+      alert("Email já existe!");
+    }
+  })
   }
+
+  // Função para fazer login de um usuário no Firebase Auth.
+  async function logarUsuario(){
+    await signInWithEmailAndPassword(auth, email, senha)
+    .then((value) => {
+      console.log("USER LOGADO COM SUCESSO");
+      console.log(value.user);
+      setUserDetail({
+        uid: value.user.uid,
+        email: value.user.email,
+    })
+      setUser(true);
+
+      setEmail('');
+      setSenha('');
+    })
+    .catch(() => {
+      console.log("ERRO AO FAZER O LOGIN");
+    })
+  }
+
+  // Função para fazer logout de um usuário no Firebase Auth.
+  async function fazerLogout(){
+    await signOut(auth)
+    setUser(false);
+    setUserDetail({});
+  }
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const { user, signIn, loadingAuth } = useContext(AuthContext)
+
+//   async function handleSignIn(e){
+//     e.preventDefault();
+
+//     if(email !== '' && password !== ''){
+//       await signIn(email, password);
+//     }
+
+//   }
 
 
   return(
@@ -175,8 +253,9 @@ export default function SignIn(){
             onChange={ (e) => setPassword(e.target.value) }
           />
 
-          <button type="submit">
-            {loadingAuth ? "Carregando..." : "Acessar"}
+          <button type="submit" onClick={logarUsuario}>
+            {/* {loadingAuth ? "Carregando..." : "Acessar"} */}
+            
           </button>
 
           <Link to="/signup">Criar uma conta</Link>
